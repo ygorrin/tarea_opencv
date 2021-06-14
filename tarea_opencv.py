f0fcc768290090020ff4feee1749637d13b695d2
Yamy <yamilagorrin@gmail.com>
2021-06-14 09:38:33 -0400

    Open CV con WebCam

--- tarea_opencv.py
+++ tarea_opencv.py
@@ -1,5 +1,6 @@
-
+#Picture
 import cv2
+import sys
 
 #image = cv2.imread("abba.png")
 #cv2.imshow("Faces found", image)
@@ -24,7 +25,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.1,
+    scaleFactor=1.2,
     minNeighbors=5,
     minSize=(30, 30),
     #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
@@ -42,3 +43,41 @@ cv2.waitKey(0)
 image = cv2.imread("imagen")
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
+
+#Video
+
+import cv2
+import sys
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
